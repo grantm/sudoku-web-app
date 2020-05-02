b3e5f1b391110a4d554b196533d1bd9334035572
Grant McLean <grant@catalyst.net.nz>
2020-05-02 22:20:58 +1200

    update URL in 'About' modal

--- src/components/modal/modal-about.js
+++ src/components/modal/modal-about.js
@@ -5,7 +5,7 @@ export default function ModalAbout({modalHandler}) {
     return (
         <div className="modal about">
             <h1>About this app</h1>
-            <p>This Sudoku web application was created by <a href="http://grantm.github.io/">Grant McLean</a>.</p>
+            <p>This Sudoku web application was created by <a href="https://grantm.github.io/">Grant McLean</a>.</p>
             <p>It is <a href="https://www.fsf.org/about/what-is-free-software">free software</a>{' '}
             which you can use, copy, modify and share under the terms of the
             GNU Affero General Public License version 3 (<a href="https://opensource.org/licenses/AGPL-3.0"
