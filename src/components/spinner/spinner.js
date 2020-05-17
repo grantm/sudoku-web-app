9c841a6a96c50be284f59f379a288cb7e6a96925
Grant McLean <grant@catalyst.net.nz>
2020-05-17 22:05:36 +1200

    add a "spinner" component
    - Adapted from: https://tobiasahlin.com/spinkit/ by Tobias Ahlin Bjerrome.

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './spinner.css';
+
+function Spinner () {
+    return (
+        <div className="spinner">
+            <div /><div /><div /><div /><div /><div /><div /><div /><div />
+        </div>
+    );
+}
+
+export default Spinner;
