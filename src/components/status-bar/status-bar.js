654e260221d7ed3501bb8f0041eb6b023139191a
Grant McLean <grant@catalyst.net.nz>
2020-05-24 18:09:35 +1200

    make "Settings" a top-level UI element next to the menu button
    - For now, "Settings" is still on the menu as well.
    
    - Fixes #6

--- src/components/status-bar/status-bar.js
+++ src/components/status-bar/status-bar.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import TimerWithPause from '../timer-with-pause/timer-with-pause';
 import MenuButton from '../menu-button/menu-button';
+import SettingsButton from '../settings-button/settings-button';
 import FullscreenButton from '../fullscreen-button/fullscreen-button';
 
 import SiteDomainImg from '../../assets/site-domain.svg';
@@ -38,6 +39,7 @@ function StatusBar ({showTimer, startTime, endTime, pausedAt, menuHandler, pause
             <SiteLink />
             {timer}
             <FullscreenButton />
+            <SettingsButton menuHandler={menuHandler} />
             <MenuButton
                 initialDigits={initialDigits}
                 startTime={startTime}
