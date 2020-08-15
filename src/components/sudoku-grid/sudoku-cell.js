695d93f3de7781b31f5810cb9111b97994ce5b9a
barak <baaraak0@gmail.com>
2020-08-15 18:30:12 +0300

    add highlighted cells

--- src/components/sudoku-grid/sudoku-cell.js
+++ src/components/sudoku-grid/sudoku-cell.js
@@ -1,180 +1,209 @@
-import React from 'react';
+import React from "react";
 
-const allDigits = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const allDigits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const outerPencilOffsets = [
-    { key: 'tl', x: 18, y: 31 },
-    { key: 'tr', x: 80, y: 31 },
-    { key: 'bl', x: 18, y: 91 },
-    { key: 'br', x: 80, y: 91 },
-    { key: 'tc', x: 49, y: 31 },
-    { key: 'bc', x: 49, y: 91 },
-    { key: 'lc', x: 18, y: 61 },
-    { key: 'rc', x: 80, y: 61 },
-    { key: 'cc', x: 49, y: 61 },
+  { key: "tl", x: 18, y: 31 },
+  { key: "tr", x: 80, y: 31 },
+  { key: "bl", x: 18, y: 91 },
+  { key: "br", x: 80, y: 91 },
+  { key: "tc", x: 49, y: 31 },
+  { key: "bc", x: 49, y: 91 },
+  { key: "lc", x: 18, y: 61 },
+  { key: "rc", x: 80, y: 61 },
+  { key: "cc", x: 49, y: 61 },
 ];
 
 function hasPencilledDigit(cell, d) {
-    return cell.get('innerPencils').includes(d) || cell.get('outerPencils').includes(d);
+  return (
+    cell.get("innerPencils").includes(d) || cell.get("outerPencils").includes(d)
+  );
 }
 
-function CellBackground({cell, showPencilmarks}) {
-    const colorCode = showPencilmarks ? cell.get('colorCode') : '1';
-    return <>
-        <rect
-            className={`cell-bg color-code-${colorCode}`}
-            x={cell.get('x')}
-            y={cell.get('y')}
-            width="100"
-            height="100"
-        />
-        <rect
-            className="cell-bg-overlay"
-            x={cell.get('x')}
-            y={cell.get('y')}
-            width="100"
-            height="100"
-        />
+function CellBackground({ cell, showPencilmarks }) {
+  const colorCode = showPencilmarks ? cell.get("colorCode") : "1";
+  return (
+    <>
+      <rect
+        className={`cell-bg color-code-${colorCode}`}
+        x={cell.get("x")}
+        y={cell.get("y")}
+        width="100"
+        height="100"
+      />
+      <rect
+        className="cell-bg-overlay"
+        x={cell.get("x")}
+        y={cell.get("y")}
+        width="100"
+        height="100"
+      />
     </>
+  );
 }
 
-function CellDigit({cell}) {
-    const digit = cell.get('digit');
-    if (digit === '0') {
-        return null;
-    }
-    return (
+function CellDigit({ cell }) {
+  const digit = cell.get("digit");
+  if (digit === "0") {
+    return null;
+  }
+  return (
+    <text
+      className="digit"
+      x={cell.get("col") * 100}
+      y={cell.get("row") * 100 + 25}
+      fontSize="72"
+      textAnchor="middle"
+    >
+      {digit}
+    </text>
+  );
+}
+
+function CellOuterPencilMarks({ cell }) {
+  const pm = cell.get("outerPencils");
+  if (cell.get("digit") !== "0" || pm.size === 0) {
+    return null;
+  }
+  let i = 0;
+  const marks = allDigits
+    .filter((d) => pm.includes(d))
+    .map((d) => {
+      const pos = outerPencilOffsets[i++];
+      return (
         <text
-            className="digit"
-            x={cell.get('col') * 100}
-            y={cell.get('row') * 100 + 25}
-            fontSize="72"
-            textAnchor="middle"
+          key={pos.key}
+          x={cell.get("x") + pos.x}
+          y={cell.get("y") + pos.y}
+          fontSize="26"
+          textAnchor="middle"
         >
-            {digit}
+          {d}
         </text>
-    );
+      );
+    });
+  return <g className="outer-pencil">{marks}</g>;
 }
 
-function CellOuterPencilMarks({cell}) {
-    const pm = cell.get('outerPencils');
-    if (cell.get('digit') !== '0' || pm.size === 0) {
-        return null;
-    }
-    let i = 0;
-    const marks = allDigits
-        .filter(d => pm.includes(d))
-        .map((d) => {
-            const pos = outerPencilOffsets[i++];
-            return (
-                <text
-                    key={pos.key}
-                    x={cell.get('x') + pos.x}
-                    y={cell.get('y') + pos.y}
-                    fontSize="26"
-                    textAnchor="middle"
-                >
-                    {d}
-                </text>
-            );
-        });
-    return <g className="outer-pencil">{marks}</g>;
+function CellInnerPencilMarks({ cell }) {
+  const pm = cell.get("innerPencils");
+  if (cell.get("digit") !== "0" || pm.size === 0) {
+    return null;
+  }
+  const digits = allDigits.filter((d) => pm.includes(d)).join("");
+  return (
+    <text
+      className="inner-pencil"
+      x={cell.get("x") + 49}
+      y={cell.get("y") + 61}
+      fontSize="26"
+      textAnchor="middle"
+    >
+      {digits}
+    </text>
+  );
 }
 
-function CellInnerPencilMarks({cell}) {
-    const pm = cell.get('innerPencils');
-    if (cell.get('digit') !== '0' || pm.size === 0) {
-        return null;
-    }
-    const digits = allDigits.filter(d => pm.includes(d)).join('');
-    return (
-        <text
-            className="inner-pencil"
-            x={cell.get('x') + 49}
-            y={cell.get('y') + 61}
-            fontSize="26"
-            textAnchor="middle"
-        >
-            {digits}
-        </text>
-    );
+function CellCover({ cell, mouseDownHandler, mouseOverHandler }) {
+  const tooltip = cell.get("errorMessage") ? (
+    <title>{cell.get("errorMessage")}</title>
+  ) : null;
+  return (
+    <rect
+      x={cell.get("x")}
+      y={cell.get("y")}
+      data-cell-index={cell.get("index")}
+      width="100"
+      height="100"
+      fill="transparent"
+      onMouseDown={mouseDownHandler}
+      onMouseOver={mouseOverHandler}
+      pointerEvents="fill"
+    >
+      {tooltip}
+    </rect>
+  );
 }
 
-function CellCover({cell, mouseDownHandler, mouseOverHandler}) {
-    const tooltip = cell.get('errorMessage')
-        ? <title>{cell.get('errorMessage')}</title>
-        : null;
-    return (
-        <rect
-            x={cell.get('x')}
-            y={cell.get('y')}
-            data-cell-index={cell.get('index')}
-            width="100"
-            height="100"
-            fill="transparent"
-            onMouseDown={mouseDownHandler}
-            onMouseOver={mouseOverHandler}
-            pointerEvents="fill"
-        >{tooltip}</rect>
-    )
+function PausedSudokuCell({ cell }) {
+  return (
+    <g className="cell">
+      <text
+        className="digit"
+        x={cell.get("col") * 100}
+        y={cell.get("row") * 100 + 25}
+        fontSize="65"
+        textAnchor="middle"
+      >
+        ?
+      </text>
+    </g>
+  );
 }
 
-
-function PausedSudokuCell({cell}) {
-    return (
-        <g className="cell">
-            <text
-                className="digit"
-                x={cell.get('col') * 100}
-                y={cell.get('row') * 100 + 25}
-                fontSize="65"
-                textAnchor="middle"
-            >?</text>
-        </g>
-    );
+function SudokuCell({
+  cell,
+  showPencilmarks,
+  matchDigit,
+  isPaused,
+  mouseDownHandler,
+  mouseOverHandler,
+  selectedRow,
+  selectedCol,
+}) {
+  if (isPaused) {
+    return <PausedSudokuCell cell={cell} />;
+  }
+  const classes = ["cell"];
+  if (cell.get("isGiven")) {
+    classes.push("given");
+  }
+  if (cell.get("isSelected")) {
+    classes.push("selected");
+  }
+  if (cell.get("errorMessage") !== undefined) {
+    classes.push("error");
+  }
+  const digit = cell.get("digit");
+  if (matchDigit !== "0") {
+    if (
+      digit === matchDigit ||
+      (showPencilmarks && hasPencilledDigit(cell, matchDigit))
+    ) {
+      classes.push("matched");
+    }
+  }
+  if (cell.get("row") === selectedRow) {
+    classes.push("highlight");
+  }
+  if (cell.get("col") === selectedCol) {
+    classes.push("highlight");
+  }
+  const outerPencilmarks = showPencilmarks ? (
+    <CellOuterPencilMarks cell={cell} />
+  ) : null;
+  const innerPencilmarks = showPencilmarks ? (
+    <CellInnerPencilMarks cell={cell} />
+  ) : null;
+  return (
+    <g
+      className={classes.join(" ")}
+      data-cell-index={cell.get("index")}
+      data-row={cell.get("row")}
+      data-col={cell.get("col")}
+      data-box={cell.get("box")}
+      data-ring={cell.get("ring")}
+    >
+      <CellBackground cell={cell} showPencilmarks={showPencilmarks} />
+      <CellDigit cell={cell} />
+      {outerPencilmarks}
+      {innerPencilmarks}
+      <CellCover
+        cell={cell}
+        mouseDownHandler={mouseDownHandler}
+        mouseOverHandler={mouseOverHandler}
+      />
+    </g>
+  );
 }
 
-
-function SudokuCell({cell, showPencilmarks, matchDigit, isPaused, mouseDownHandler, mouseOverHandler}) {
-    if (isPaused) {
-        return <PausedSudokuCell cell={cell} />
-    }
-    const classes = [ 'cell' ];
-    if (cell.get('isGiven')) {
-        classes.push('given');
-    }
-    if (cell.get('isSelected')) {
-        classes.push('selected');
-    }
-    if (cell.get('errorMessage') !== undefined) {
-        classes.push('error');
-    }
-    const digit = cell.get('digit')
-    if (matchDigit !== '0') {
-        if (digit === matchDigit || (showPencilmarks && hasPencilledDigit(cell, matchDigit))) {
-            classes.push('matched');
-        }
-    }
-    const outerPencilmarks = showPencilmarks ? <CellOuterPencilMarks cell={cell} /> : null;
-    const innerPencilmarks = showPencilmarks ? <CellInnerPencilMarks cell={cell} /> : null;
-    return (
-        <g className={classes.join(' ')}
-            data-cell-index={cell.get('index')}
-            data-row={cell.get('row')}
-            data-col={cell.get('col')}
-            data-box={cell.get('box')}
-            data-ring={cell.get('ring')}
-        >
-            <CellBackground cell={cell} showPencilmarks={showPencilmarks} />
-            <CellDigit cell={cell} />
-            {outerPencilmarks}
-            {innerPencilmarks}
-            <CellCover
-                cell={cell}
-                mouseDownHandler={mouseDownHandler}
-                mouseOverHandler={mouseOverHandler}
-            />
-        </g>
-    );
-}
-
-export default React.memo( SudokuCell );
+export default React.memo(SudokuCell);
--- src/components/sudoku-grid/sudoku-grid.js
+++ src/components/sudoku-grid/sudoku-grid.js
@@ -1,92 +1,119 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './sudoku-grid.css';
+import "./sudoku-grid.css";
 
-import { SETTINGS } from '../../lib/sudoku-model.js';
+import { SETTINGS } from "../../lib/sudoku-model.js";
 
-import SudokuCell from './sudoku-cell';
-import GridLines from './grid-lines.js';
+import SudokuCell from "./sudoku-cell";
+import GridLines from "./grid-lines.js";
 
-
-function indexFromTouchEvent (e) {
-    const t = (e.touches || {})[0];
-    if (t) {
-        let index = t.target.dataset.cellIndex;
-        if (t.pageX) {
-            const el = document.elementFromPoint(t.pageX, t.pageY);
-            if (el && el !== t.target) {
-                index = el.dataset.cellIndex;
-            }
-        }
-        if (index !== undefined) {
-            return parseInt(index, 10);
-        }
+function indexFromTouchEvent(e) {
+  const t = (e.touches || {})[0];
+  if (t) {
+    let index = t.target.dataset.cellIndex;
+    if (t.pageX) {
+      const el = document.elementFromPoint(t.pageX, t.pageY);
+      if (el && el !== t.target) {
+        index = el.dataset.cellIndex;
+      }
     }
-    return;
+    if (index !== undefined) {
+      return parseInt(index, 10);
+    }
+  }
+  return;
 }
 
-function useCellTouch (inputHandler) {
-    const [lastCellTouched, setLastCellTouched] = useState(false);
-    return (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        const eventType = e.type;
-        if (eventType === 'touchend') {
-            setLastCellTouched(undefined);
-            return
-        }
-        const cellIndex = indexFromTouchEvent(e);
-        if (cellIndex !== undefined && cellIndex !== lastCellTouched) {
-            if (eventType === 'touchstart') {
-                inputHandler({type: 'cellTouched', cellIndex, value: cellIndex, source: 'touch'});
-            }
-            else if (eventType === 'touchmove') {
-                inputHandler({type: 'cellSwipedTo', cellIndex, value: cellIndex, source: 'touch'});
-            }
-            setLastCellTouched(cellIndex);
-            // console.log(`${eventType} event on cell #${cellIndex}`);
-        }
-    };
+function useCellTouch(inputHandler) {
+  const [lastCellTouched, setLastCellTouched] = useState(false);
+  return (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const eventType = e.type;
+    if (eventType === "touchend") {
+      setLastCellTouched(undefined);
+      return;
+    }
+    const cellIndex = indexFromTouchEvent(e);
+    if (cellIndex !== undefined && cellIndex !== lastCellTouched) {
+      if (eventType === "touchstart") {
+        inputHandler({
+          type: "cellTouched",
+          cellIndex,
+          value: cellIndex,
+          source: "touch",
+        });
+      } else if (eventType === "touchmove") {
+        inputHandler({
+          type: "cellSwipedTo",
+          cellIndex,
+          value: cellIndex,
+          source: "touch",
+        });
+      }
+      setLastCellTouched(cellIndex);
+      // console.log(`${eventType} event on cell #${cellIndex}`);
+    }
+  };
 }
 
+function SudokuGrid({
+  grid,
+  gridId,
+  dimensions,
+  isPaused,
+  mouseDownHandler,
+  mouseOverHandler,
+  inputHandler,
+}) {
+  const settings = grid.get("settings");
+  const highlightMatches = settings[SETTINGS.highlightMatches];
+  const showPencilmarks = grid.get("showPencilmarks");
+  const matchDigit = highlightMatches ? grid.get("matchDigit") : undefined;
+  const rawTouchHandler = useCellTouch(inputHandler);
+  const focusedCell = grid
+    .get("cells")
+    .filter((c) => c.get("index") === grid.get("focusIndex"))
+    .get(0);
 
-function SudokuGrid({grid, gridId, dimensions, isPaused, mouseDownHandler, mouseOverHandler, inputHandler}) {
-    const settings = grid.get('settings');
-    const highlightMatches = settings[SETTINGS.highlightMatches];
-    const showPencilmarks = grid.get('showPencilmarks');
-    const matchDigit = highlightMatches ? grid.get('matchDigit') : undefined;
-    const rawTouchHandler = useCellTouch(inputHandler);
-    const cellContents = grid.get('cells').toArray().map((c) => {
-        return (
-            <SudokuCell
-                key={c.get('location')}
-                cell={c}
-                showPencilmarks={showPencilmarks}
-                matchDigit={matchDigit}
-                isPaused={isPaused}
-                mouseDownHandler={mouseDownHandler}
-                mouseOverHandler={mouseOverHandler}
-            />
-        )
+  const cellContents = grid
+    .get("cells")
+    .toArray()
+    .map((c) => {
+      return (
+        <SudokuCell
+          key={c.get("location")}
+          cell={c}
+          showPencilmarks={showPencilmarks}
+          matchDigit={matchDigit}
+          isPaused={isPaused}
+          mouseDownHandler={mouseDownHandler}
+          mouseOverHandler={mouseOverHandler}
+          selectedRow={focusedCell?.get("row")}
+          selectedCol={focusedCell?.get("col")}
+        />
+      );
     });
-    return (
-        <div className="sudoku-grid"
-            id={gridId || null}
-            onTouchStart={rawTouchHandler}
-            onTouchEnd={rawTouchHandler}
-            onTouchMove={rawTouchHandler}
-        >
-            <svg version="1.1"
-                style={{width: dimensions.gridLength}}
-                viewBox="0 0 1000 1000"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <rect className="grid-bg" width="100%" height="100%" />
-                {cellContents}
-                <GridLines />
-            </svg>
-        </div>
-    );
+  return (
+    <div
+      className="sudoku-grid"
+      id={gridId || null}
+      onTouchStart={rawTouchHandler}
+      onTouchEnd={rawTouchHandler}
+      onTouchMove={rawTouchHandler}
+    >
+      <svg
+        version="1.1"
+        style={{ width: dimensions.gridLength }}
+        viewBox="0 0 1000 1000"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect className="grid-bg" width="100%" height="100%" />
+        {cellContents}
+        <GridLines />
+      </svg>
+    </div>
+  );
 }
 
 export default SudokuGrid;
