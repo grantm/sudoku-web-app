8d6d5660d1b016d9fc5c49431aa8346fba7589d7
Grant McLean <grant@mclean.net.nz>
2021-08-16 22:44:15 +1200

    make a less confusing version of saved puzzle modal for just one puzzle
    - On a user's first encounter with the SavedPuzzles modal, they will most
      likely only have one saved puzzle.  So the wording about selecting from
      the list might be a bit confusing (according to test user feedback).
    
    - This commit adds a simplified version of the modal for the case where
      there is only one saved puzzle.

--- src/components/modal/modal-saved-puzzles.js
+++ src/components/modal/modal-saved-puzzles.js
@@ -4,15 +4,27 @@ import SavedPuzzleGrid from "../saved-puzzle/saved-puzzle-grid";
 import SavedPuzzleMetadata from "../saved-puzzle/saved-puzzle-metadata";
 
 
+function puzzleLink(puzzleState) {
+    const {initialDigits, difficultyLevel} = puzzleState;
+    const diffParam = difficultyLevel ? `&d=${difficultyLevel}` : '';
+    return `./?s=${initialDigits}${diffParam}&r=1`;
+}
+
+
 function SavedPuzzle({puzzleState, showRatings, discardHandler}) {
     const {
-        puzzleStateKey, initialDigits, difficultyLevel,
+        puzzleStateKey, difficultyLevel,
         startTime, elapsedTime, lastUpdatedTime
     } = puzzleState;
-    const diffParam = difficultyLevel ? `&d=${difficultyLevel}` : '';
-    const puzzleLink = `./?s=${initialDigits}${diffParam}&r=1`;
+    const discardButton = discardHandler
+        ? <button
+            onClick={discardHandler}
+            data-puzzle-state-key={puzzleStateKey}
+            title="Discard this saved puzzle"
+          >×</button>
+        : null;
     return <div className="saved-puzzle">
-        <a className="puzzle-selector" href={puzzleLink}>
+        <a className="puzzle-selector" href={puzzleLink(puzzleState)}>
             <SavedPuzzleGrid
                 puzzleState={puzzleState}
                 showRatings={showRatings}
@@ -25,13 +37,48 @@ function SavedPuzzle({puzzleState, showRatings, discardHandler}) {
             elapsedTime={elapsedTime}
         />
         <div className="discard">
-            <button
-                onClick={discardHandler}
-                data-puzzle-state-key={puzzleStateKey}
-                title="Discard this saved puzzle"
-            >×</button>
+            {discardButton}
         </div>
     </div>;
+}
+
+
+function OneSavedPuzzle({puzzleState, showRatings, discardHandler, cancelHandler}) {
+    return <>
+        <h1>Continue or discard?</h1>
+        <p>You can either return to this saved puzzle and<br />
+        pick up where you left off, or you can discard it.</p>
+        <SavedPuzzle
+            puzzleState={puzzleState}
+            showRatings={showRatings}
+        />
+        <div className="buttons">
+            <a className="btn primary" href={puzzleLink(puzzleState)}>Continue</a>
+            <button className="cancel" onClick={discardHandler}
+                data-puzzle-state-key={puzzleState.puzzleStateKey}>Discard</button>
+            <button className="cancel" onClick={cancelHandler}>Cancel</button>
+        </div>
+    </>;
+}
+
+
+function SavedPuzzleList({savedPuzzles=[], showRatings, discardHandler, cancelHandler}) {
+    const puzzles = savedPuzzles.map((puzzleState, i) => {
+        return <SavedPuzzle
+            key={puzzleState.puzzleStateKey}
+            puzzleState={puzzleState}
+            showRatings={showRatings}
+            discardHandler={discardHandler}
+        />
+    });
+    return <>
+        <h1>Incomplete puzzles</h1>
+        <p>Select a puzzle to return to, or discard a partially completed puzzle</p>
+        {puzzles}
+        <div className="buttons">
+            <button className="cancel" onClick={cancelHandler}>Cancel</button>
+        </div>
+    </>;
 }
 
 
@@ -45,22 +92,26 @@ function ModalSavedPuzzles({modalState, modalHandler}) {
             puzzleStateKey,
         });
     };
-    const puzzles = (savedPuzzles || []).map((puzzleState, i) => {
-        return <SavedPuzzle
-            key={puzzleState.puzzleStateKey}
-            puzzleState={puzzleState}
-            showRatings={showRatings}
-            discardHandler={discardHandler}
-        />
-    });
+    const modalContent = (savedPuzzles || []).length === 1
+        ? (
+            <OneSavedPuzzle
+                puzzleState={savedPuzzles[0]}
+                showRatings={showRatings}
+                discardHandler={discardHandler}
+                cancelHandler={cancelHandler}
+            />
+           )
+         : (
+             <SavedPuzzleList
+                 savedPuzzles={savedPuzzles}
+                 showRatings={showRatings}
+                 discardHandler={discardHandler}
+                 cancelHandler={cancelHandler}
+             />
+           );
     return (
         <div className="modal saved-puzzles">
-            <h1>Incomplete puzzles</h1>
-            <p>Select a puzzle to return to, or discard a partially completed puzzle</p>
-            {puzzles}
-            <div className="buttons">
-                <button className="cancel" onClick={cancelHandler}>Cancel</button>
-            </div>
+            {modalContent}
         </div>
     );
 }
