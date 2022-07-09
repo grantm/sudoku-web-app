9eb3ccbb367c4e0c8c166ea6ce1df10bde688ce0
Grant McLean <grant@mclean.net.nz>
2022-07-10 09:46:55 +1200

    make fullscreen button work on Safari
    - Thought this was broken by a recent commit, but it looks like it
      probably never worked.
    
    - Safari currently needs the 'webkit' prefix on property and method names.
    
    - If the requestFullscreen method is not found with or without the prefix
      then the fullscreen button is not displayed.

--- src/components/buttons/fullscreen-button.js
+++ src/components/buttons/fullscreen-button.js
@@ -2,30 +2,55 @@ import { useState, useEffect } from 'react';
 
 import ButtonIcon from '../svg-sprites/button-icon';
 
+let fsApi = null;
+
 function FullscreenButton () {
-    const [fsEnabled, setFsEnabled] = useState(!!document.fullscreenElement);
+    fsApi ||= determineFullscreenApi();
+    const [fsEnabled, setFsEnabled] = useState(fsApi && !!document[fsApi.ELEMENT_PROP_NAME]);
     const title = fsEnabled ? 'Exit full screen' : 'Full screen';
     const clickHandler = (e) => {
         e.currentTarget.blur();
-        return !!document.fullscreenElement
-            ? document.exitFullscreen()
-            : document.body.requestFullscreen();
+        return !!document[fsApi.ELEMENT_PROP_NAME]
+            ? document[fsApi.EXIT_METHOD_NAME]()
+            : document.body[fsApi.ENTER_METHOD_NAME]();
     };
     useEffect(
         () => {
-            const resizeHandler = (e) => { setFsEnabled(window.document.fullscreen); };
-            window.addEventListener('fullscreenchange', resizeHandler);
+            const resizeHandler = (e) => { setFsEnabled(!!document[fsApi.ELEMENT_PROP_NAME]); };
+            document.body.addEventListener('fullscreenchange', resizeHandler);
             return () => {
-                window.removeEventListener('fullscreenchange', resizeHandler);
+                document.body.removeEventListener('fullscreenchange', resizeHandler);
             }
         },
-        [setFsEnabled]
+        [fsApi, setFsEnabled]
     );
-    return (
-        <button id="fullscreen-button" type="button" title={title} onClick={clickHandler}>
-            {fsEnabled ? <ButtonIcon name="exit-fullscreen" /> : <ButtonIcon name="enter-fullscreen" />}
-        </button>
-    );
+    return fsApi
+        ? (
+            <button id="fullscreen-button" type="button" title={title} onClick={clickHandler}>
+                {fsEnabled ? <ButtonIcon name="exit-fullscreen" /> : <ButtonIcon name="enter-fullscreen" />}
+            </button>
+        )
+        : null;
+}
+
+function determineFullscreenApi() {
+    if (document.body.requestFullscreen) {
+        return {
+            ELEMENT_PROP_NAME: "fullscreenElement",
+            ENTER_METHOD_NAME: "requestFullscreen",
+            EXIT_METHOD_NAME: "exitFullscreen",
+        }
+    }
+    else if (document.body.webkitRequestFullscreen) {
+        return {
+            ELEMENT_PROP_NAME: "webkitFullscreenElement",
+            ENTER_METHOD_NAME: "webkitRequestFullscreen",
+            EXIT_METHOD_NAME: "webkitExitFullscreen",
+        }
+    }
+    else {
+        return null;
+    }
 }
 
 export default FullscreenButton;
